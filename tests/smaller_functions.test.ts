54983f6c00c5b2c4661f33b7e37e95ca87921380
d-mv <d.dash.mv@gmail.com>
2020-02-16 12:32:27 +0200

    test: collectable address caching

--- tests/smaller_functions.test.ts
+++ tests/smaller_functions.test.ts
@@ -2,7 +2,7 @@ import Service, { DebugLevels, Responses, Event } from '../src'
 import { TEXT, validBitcoinAddresses, listOfStatusKeys, typeOfStatusValues } from '../src/data'
 import { makeStringFromTemplate } from '../src/tools'
 import { validateObject } from '../src/validators'
-import { ObjectWithStringKeysAnyValues } from '../src//types'
+import { ObjectWithStringKeysAnyValues } from '../src/types'
 
 let storedEvent: Event
 
@@ -37,7 +37,6 @@ describe('Smaller functions', () => {
     test("- returns information in 'Callback' mode", async () => {
       expect.assertions(2)
       await service.getStatus()
-      console.log(storedEvent)
       expect(storedEvent.type).toBe('service_update_status')
       const result: ObjectWithStringKeysAnyValues = storedEvent.payload
       let keysValuesCheck = true
@@ -56,7 +55,26 @@ describe('Smaller functions', () => {
       expect(keysValuesCheck).toBe(true)
     })
   })
-  describe('- getStatus', () => {
-    test("- returns information in 'Callback' mode", async () => {})
+  describe('- cached addresses functions', () => {
+    test("- doesn't cache address in case of incorrect request", async () => {
+      await service.getCollectables([validBitcoinAddresses[1]])
+      const result = service.getLastAddresses()
+      expect(result.length).toBe(0)
+    })
+
+    test('- caches address in case of correct request', async () => {
+      await service.getCollectables([validBitcoinAddresses[2]])
+      const result = service.getLastAddresses()[0]
+      expect(result).toBe(validBitcoinAddresses[2])
+    })
+    test('- clears cache', async () => {
+      expect.assertions(2)
+      await service.getCollectables([validBitcoinAddresses[2]])
+      const result = service.getLastAddresses()[0]
+      expect(result).toBe(validBitcoinAddresses[2])
+      service.clearLastAddresses()
+      const clear = service.getLastAddresses()
+      expect(clear.length).toBe(0)
+    })
   })
 })
