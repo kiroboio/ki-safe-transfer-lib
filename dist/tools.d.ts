5e06463f4dea6eaf037800e8c9486b00797cd9f7
Tal Asa <tal@kirobo.io>
2020-02-16 16:43:50 +0200

    removing dist folder from git

--- dist/tools.d.ts
+++
@@ -1,4 +0,0 @@
-import { ObjectWithStringKeysAnyValues } from './types';
-export declare const capitalize: (text: string) => string;
-export declare const makeStringFromTemplate: (template: string, params: string[]) => string;
-export declare const compareBasicObjects: (objOne: ObjectWithStringKeysAnyValues, objTwo: ObjectWithStringKeysAnyValues) => boolean;
